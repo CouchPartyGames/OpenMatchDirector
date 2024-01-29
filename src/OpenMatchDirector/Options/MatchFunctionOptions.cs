e04e1092cb7cf30ec6b08dd442bc5ae98e8848f0
CouchPartyGames <83233322+CouchPartyGames@users.noreply.github.com>
2024-01-29 18:57:51 -0500

    feat: add options

--- src/OpenMatchDirector/Options/MatchFunctionOptions.cs
+++ src/OpenMatchDirector/Options/MatchFunctionOptions.cs
@@ -1,10 +1,10 @@
 namespace OpenMatchDirector.Options;
 
-public class MatchFunctionOptions
+public sealed class MatchFunctionOptions
 {
     public const string SectionName = "MatchFunctions";
     
-    public string Name { get; }
-    public string Address { get; }
-    public int Port { get; }
+    public string Name { get; init; }
+    public string Address { get; init; }
+    public int Port { get; init; }
 }
---
+++ src/OpenMatchDirector/Options/TelemetryOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace OpenMatchDirector.Options;
+
+public sealed class TelemetryOptions
+{
+   public const string SectionName = "Telemetry";
+   
+   public bool Enabled { get; init; } = false;
+   
+   public string Endpoint { get; init; }
+}
