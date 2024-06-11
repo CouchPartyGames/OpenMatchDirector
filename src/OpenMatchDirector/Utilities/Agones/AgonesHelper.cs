d3ea6328bbd3036df2a39b072f28c0d692dc7c0b
CouchParty Games <83233322+CouchPartyGames@users.noreply.github.com>
2024-06-11 12:12:18 -0400

    chore: clean up messaging, add tostring for agones host

--- src/OpenMatchDirector/Utilities/Agones/AgonesHelper.cs
+++ src/OpenMatchDirector/Utilities/Agones/AgonesHelper.cs
@@ -2,8 +2,10 @@ namespace OpenMatchDirector.Utilities.Agones;
 
 public static class AgonesHelper
 {
-    public record AgonesHost(string Address, int Port);
+    public record AgonesHost(string Address, int Port)
+    {
+        public override string ToString() => $"{Address}:{Port}";
+    }
 
     public static AgonesHost NewFakeHost() => new AgonesHost("192.168.1.1", 5000);
-
 }
