abf234c829c464515961a7db2ea17d82de2f3e94
CouchPartyGames <83233322+CouchPartyGames@users.noreply.github.com>
2024-06-11 20:42:34 -0400

    chore: name change

--- src/OpenMatchDirector/Clients/OpenMatchBackend/Options/BackendOptions.cs
+++ src/OpenMatchDirector/Clients/OpenMatchBackend/Options/BackendOptions.cs
@@ -2,7 +2,7 @@ namespace OpenMatchDirector.Clients.OpenMatchBackend;
 
 public sealed class BackendOptions
 {
-    public const string SectionName = "Backend";
+    public const string SectionName = "OpenMatchBackend";
 
     public string BackendServiceAddr { get; init; } = "http://open-match-backend.open-match.svc.cluster.local:50505";
 }
